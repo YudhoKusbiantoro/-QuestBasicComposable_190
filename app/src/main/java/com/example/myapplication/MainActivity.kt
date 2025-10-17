ee47ff0312522188da0df1f9bf0e38c041302d12
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-10-17 17:15:32 +0700

    memasukan composable TataLetakColumnRow yang di ingin ke main dan menciba dirun

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TataLetakBox(
+                    TataLetakColumnRow(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
